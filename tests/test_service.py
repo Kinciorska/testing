c08f27c169f30e61ad1cf69a0b769fce5d618713
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-01-17 13:39:51 +0100

    feature - added API mocking

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,6 +1,8 @@
 import pytest
 import unittest.mock as mock
 
+import requests
+
 import source.service as service
 
 
@@ -9,3 +11,22 @@ def test_get_user_from_db(mock_get_user_from_db):
     mock_get_user_from_db.return_value = 'Mocked Alice'
     user_name = service.get_user_from_db(1)
     assert user_name == 'Mocked Alice'
+
+
+@mock.patch('requests.get')
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
+
+
+@mock.patch('requests.get')
+def test_get_users_status_code_error(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 500
+    mock_get.return_value = mock_response
+    with pytest.raises(requests.HTTPError):
+        service.get_users()
